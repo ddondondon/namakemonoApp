fd7075c3f2222a0614e02e7dc1ac1b21f0367042
ddondondon <ddondondon305@gmail.com>
2025-02-05 11:02:40 +0900

    設定ファイル修正(EC2)

--- front/app/vue-project/vue.config.js
+++ front/app/vue-project/vue.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  devServer: {
+    allowedHosts: 'all',
+  },
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
