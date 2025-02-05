194314432a8f3b16bad84551b44fde8b74914fe7
ddon <ddon@ddonnoMacBook-Air.local>
2025-02-05 10:31:22 +0900

    設定ファイル修正

--- front/app/vue-project/vue.config.js
+++ front/app/vue-project/vue.config.js
@@ -2,9 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  devServer: {
+    allowedHosts: 'all',
+  },
+
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+                        // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+                }
   }
 })
