c9f01f630b3eae92894f1430d554983ad3c9c6eb
ddondondon <ddondondon305@gmail.com>
2025-02-07 10:24:11 +0900

    プロキシ設定修正(EC2)

--- front/app/vue-project/src/plugins/vuetify.js
+++ front/app/vue-project/src/plugins/vuetify.js
@@ -9,12 +9,7 @@ import * as components from 'vuetify/components'
 import * as labs from 'vuetify/labs/components'
 import * as directives from 'vuetify/directives'
 
-// v-calendar 追加
-import { VCalendar } from 'vuetify/labs/VCalendar'
-
 export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-  // v-calendar 追加
   {
     components: {
         ...components,  // 標準コンポーネント (v-sheet など)
